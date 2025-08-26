4ca69dd4565722ce9ec9897dfcfb229464d7eba0
phimtarwat <phimtarwat@gmail.com>
2025-08-26 13:59:27 +0700

    Update check.js

--- api/check.js
+++ api/check.js
@@ -2,7 +2,7 @@ module.exports = async (req, res) => {
   const { user_id, token, mode } = req.query;
   const fingerprint = req.headers["user-agent"] || "unknown";
 
-  const scriptUrl = "https://script.google.com/macros/s/AKfycbxcXWJIhE2kAzNQ8vekU37YMNyaLUx2nKJVF7C4puc0EvepkONxBasjOxhMuv1svX4UWw/exec";
+  const scriptUrl = "https://script.google.com/macros/s/AKfycbwNHPgVP7sE-iVQtHYadPXJvhekuxLeu6LshZSLo_DGWohiGmVmWMFpR9SlIHtK9stiEA/exec";
   const url = `${scriptUrl}?user_id=${user_id}&token=${token}&mode=${mode}&fingerprint=${encodeURIComponent(fingerprint)}`;
 
   try {
