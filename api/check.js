df208ee4f748975b48866af83a905d80ab1f21d9
phimtarwat <phimtarwat@gmail.com>
2025-08-26 14:51:40 +0700

    Update check.js

--- api/check.js
+++ api/check.js
@@ -1,15 +1,14 @@
-module.exports = async (req, res) => {
-  const { user_id, token, mode } = req.query;
-  const fingerprint = req.headers["user-agent"] || "unknown";
+export default async function handler(req, res) {
+  const { user_id, token, mode, fingerprint } = req.query;
 
-  const scriptUrl = "https://script.google.com/macros/s/AKfycbwNHPgVP7sE-iVQtHYadPXJvhekuxLeu6LshZSLo_DGWohiGmVmWMFpR9SlIHtK9stiEA/exec";
-  const url = `${scriptUrl}?user_id=${user_id}&token=${token}&mode=${mode}&fingerprint=${encodeURIComponent(fingerprint)}`;
+  const scriptUrl = "https://script.google.com/macros/s/AKfycbwS2DgMBTJJOzrXkyknOnc-uE-PjBHmSoOXpLBZt-YcdK2ZIQioA-Z55952Vt1GabuYnQ/exec";
+  const url = `${scriptUrl}?user_id=${encodeURIComponent(user_id)}&token=${encodeURIComponent(token)}&mode=${encodeURIComponent(mode)}&fingerprint=${encodeURIComponent(fingerprint || "unknown")}`;
 
   try {
     const response = await fetch(url);
     const data = await response.json();
     res.status(200).json(data);
   } catch (err) {
-    res.status(500).json({ error: "Proxy Error", details: err.message });
+    res.status(500).json({ status: "error", message: err.message });
   }
-};
+}
